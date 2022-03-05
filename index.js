cea9e8ab53ce1ba8a91e237da8f021ebee4bf2b4
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-03-04 21:30:11 -0800

    fixed code

--- index.js
+++ index.js
@@ -1,54 +1,227 @@
-// Packages needed for this application
+// Required packages 
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// Link to page creation
+const generateHTML = require("./src/generateHTML");
+
+// Team profiles
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+// Team array
+const teamArray = [];
 
 
-//AS A manager
-//I WANT to generate a webpage that displays my team's basic info
-//SO THAT I have quick access to their emails and GitHub profiles
+// Start of team manager prompts, includes following information: team manager’s name, employee ID, email address, and office number
+const addManager = () => {
+    return inquirer.prompt ([
+        {
+                type: "input",
+                name: "name",
+                message: "Who is the team manager?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the team manager's name.");
+                        return false;
+                   }
+                }
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the team manager's employee ID?",
+                validate: idInput => {
+                    if (isNaN(idInput)) {
+                        console.log("You must enter the employee ID.");
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the the team manager's email address?",
+                validate: email => {
+                    valid = /^\w+([\.-]?\w+)*@\w+)*(\.\w{2,3})+$/.test(email)
+                    if (valid) {
+                        return true;
+                    } else {
+                        console.log("You must enter a valid email address.");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the team manager's office number?",
+                validate: officeNumberInput => {
+                    if (isNaN(officeNumberInput)) {
+                        console.log("Please enter the office number.");
+                        return false;
+                    } else {
+                        return true;
+                    }
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-// WHEN I start the application
+                }
+            }
+        ])
+        // Add the new team manager information and push it to the teamArray
+        .then(managerInput => {
+            const {name, id, email, officeNumber} = managerInput;
+            const manager = new Manager(name, id, email, officeNumber);
+
+            teamArray.push(manager);
+            console.log(manager);
+        })
+};
+
+// When the team manager is added, the user is presented with the option to add a new employee to the team, 
+// i.e., engineer or intern, or to finish building the team
+const addEmployee = () => {
+    console.log(`Adding employees to the team`);
+
+    return inquirer.prompt ([
+        {
+                type: "list",
+                name: "role",
+                message: "Please select the employee's role.",
+                choices: ["Engineer", "Intern"]
+            },
+            {
+                // When user selects a role, prompts for the role start
+                // Prompts for engineer includes: employee's name, ID, email, and GitHub username
+                // Prompts for intern includes: employee's name, ID, email, and school
+                // After entering employee information, user is taken back to the menu
+
+                type: "input",
+                name: "name",
+                message: "What is the employee's name?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the employee's name.");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the employee's ID number?",
+                validate: idInput => {
+                    if (isNaN(idInput)) {
+                        console.log("You must enter the employee ID.")
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the employee's email address?",
+                validate: email => {
+                    valid = /^\w+([\.-]?\w+)*@\w+)*(\.\w{2,3})+$/.test(email)
+                    if (valid) {
+                        return true;
+                    } else {
+                        console.log("You must enter a valid email address.")
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is the engineer's GitHub username?",
+                when: (input) => input.role === "Engineer",
+                validate: githubInput => {
+                    if (githubInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the GitHub username.")
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Please enter the intern's school.",
+                when: (input) => input.role === "Intern",
+                validate: schoolInput => {
+                    if (schoolInput) {
+                        return true;
+                    } else {
+                        console.log("You must enter the school.")
+                    }
+                }
+            },
+            {
+                type: "confirm",
+                name: "confirmAddEmployee",
+                message: "Would you like to add more team members?",
+                default: false
+            }
+        ])
+        // Add the new employee information and push it to teamArray
+        .then(employeeData => {
+
+            // Data for employee types
+            let {name, id, email, role, github, school, confirmAddEmployee} = employeeData;
+            let employee;
+
+            if (role === "Engineer") {
+                employee = new Engineer(name, id, email, github);
+                console.log(employee);
+            } else if (role === "Intern") {
+                employee = new Intern(name, id, emai, school);
+                console.log(employee);
+            }
+
+            teamArray.push(employee);
+            if (confirmAddEmployee) {
+                return addEmployee(teamArray);
+            } else {
+                return teamArray;
+            }
+        })
+};
+// When user decides to finish building his/her team, the user will exit the application, and the HTML is generated
+// Function to generate HTML page file using fs
+const writeFile = data => {
+    fs.writeFile("./dist/index.html", data, err => {
+        // If there is an error
+        if (err) {
+            console.log(err);
+            return;
+            //When the profile has been created
+        } else {
+            console.log("All done! Your team profile has been created!")
+        }
+    })
+};
+
+addManager()
+    .then(addEmployee)
+    .then(teamArray => {
+        return generateHTML(teamArray);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 
-// Question prompts for the Team Manager
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
 
 
-// Question prompts for the Engineer
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-
-// Question prompts for the Intern
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated*
-
-
-GIVEN a command-line application that accepts user input
-WHEN I am prompted for my team members and their information
-THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-WHEN I click on an email address in the HTML
-THEN my default email program opens and populates the TO field of the email with the address
-WHEN I click on the GitHub username
-THEN that GitHub profile opens in a new tab
-WHEN I start the application
-THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-WHEN I enter the team manager’s name, employee ID, email address, and office number
-THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-WHEN I select the engineer option
-THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-WHEN I select the intern option
-THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-WHEN I decide to finish building my team
-THEN I exit the application, and the HTML is generated*
