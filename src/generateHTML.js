d6bf6dbe113a5498c78443a785e74d02ff4a6342
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-03-10 07:47:00 -0800

    fixed code, added bootstrap and css link

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,18 +1,17 @@
 // Create Team Manager card
 const generateManager = function (manager) {
     return `
-    <div class="">
-        <div class="card ">
+    <div class="col-4 mt-4">
+        <div class="card h-100">
             <div class="card-header">
-                <h2>${manager.name}</h2>
-                <h3>Manager</h3>
+                <h3 class="name">${manager.name}</h3>
+                <h4 class="role"><img src="https://img.icons8.com/cute-clipart/30/000000/rocket.png"/>Manager</h4>
             </div>
 
             <div class="card-body">
                 <p class="id">ID: ${manager.id}</p>
                 <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-                <p class="office-number">Office Number:${manager.officeNumber}</p>
-
+                <p class="office-number">Office Number: ${manager.officeNumber}</p>
             </div>
            
         </div>
@@ -24,11 +23,11 @@ const generateManager = function (manager) {
 const generateEngineer = function (engineer) {
     return `
 
-    <div class="">
-        <div class="card ">
+    <div class="col-4 mt-4">
+        <div class="card h-100">
             <div class="card-header">
-                <h2>${engineer.name}</h2>
-                <h3>Engineer</h3>
+                <h3 class="name">${engineer.name}</h3>
+                <h4 class="role"><img src="https://img.icons8.com/external-flaticons-flat-flat-icons/30/000000/external-laptop-digital-nomading-relocation-flaticons-flat-flat-icons.png"/> Engineer</h4>
             </div>
 
             <div class="card-body">
@@ -45,18 +44,17 @@ const generateEngineer = function (engineer) {
 // Create Intern card
 const generateIntern = function (intern) {
     return `
-    <div class="">
-        <div class="card ">
+    <div class="col-4 mt-4">
+        <div class="card h-100">
             <div class="card-header">
-                <h2>${intern.name}</h2>
-                <h3>Intern</h3>
+                <h3 class="name">${intern.name}</h3>
+                <h4 class="role"><img src="https://img.icons8.com/external-smashingstocks-flat-smashing-stocks/30/000000/external-robot-robots-smashingstocks-flat-smashing-stocks-69.png"/> Intern</h4>
             </div>
 
             <div class="card-body">
                 <p class="id">ID: ${intern.id}</p>
                 <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
-                <p class="school">School:${intern.school}</p>
-
+                <p class="school">School: ${intern.school}</p>
             </div>
            
         </div>
@@ -113,25 +111,31 @@ const generateTeamPage = function (employeeCards) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel ="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" />
+    <link rel ="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css" />
 </head>
 
 <body>
     <header>
         <nav class="navbar" id="navbar">
-            <span class="navbar-brand" id="navbar-text">Team Profile</span>
+            <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
         </nav>
     </header>
 
     <main>
         <div class="container">
             <div class="row justify-content-center" id="employee-cards">
-                <!-- Employee Cards -->
+                
+            <!-- Employee Cards -->
                 ${employeeCards}
+                
             </div>
         </div>
     </main>
+    <footer>
+    <br>
+    <p class="icon-link">Icons by <a href="https://icons8.com/">Icons8</a></p>
+    </footer>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
@@ -145,4 +149,3 @@ const generateTeamPage = function (employeeCards) {
 
 // Export to index
 module.exports = generateHTML;
-
